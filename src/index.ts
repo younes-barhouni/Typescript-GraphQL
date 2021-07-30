5af3d839b3135bd7f3a0d87a6d9cbb56cf325937
BARHOUNI <younes-barhouni@users.noreply.github.com>
2021-07-30 14:03:19 +0200

    graphql hello word

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import {
+    ApolloServerPluginLandingPageGraphQLPlayground
+  } from "apollo-server-core";
+import { buildSchema } from "type-graphql";
+import { HelloWorldResolver } from "./resolvers/HelloWorld.resolver";
+
+(async () => {
+  const app = express();
+
+  await createConnection();
+
+  const apolloServer = new ApolloServer({
+    plugins: [
+      ApolloServerPluginLandingPageGraphQLPlayground(),
+    ],
+    schema: await buildSchema({
+      resolvers: [HelloWorldResolver]
+    }),
+    context: ({ req, res }) => ({ req, res })
+  });
+
+  await apolloServer.start()
+  apolloServer.applyMiddleware({ app, cors: false });
+
+  app.listen(4000, () => {
+    console.log("express server started");
+  });
+
+})();
